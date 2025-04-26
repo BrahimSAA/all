a12c99a344cf023f8dd27f0f7289d2690c8a7e96
Brahim SAADA <brahim.saada@uit.ac.ma>
2025-04-26 16:46:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -139,6 +139,13 @@ const courses = [
         image: "https://images.unsplash.com/photo-1573164713988-8665fc963095?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1469&q=80",
         link: "https://brahimsaa.github.io/php",
         category: "web development"
+    },
+    {
+    title: "Windows Server 2022",
+    description: "Comprehensive training on Windows Server installation, configuration, and administration.",
+    image: "https://images.unsplash.com/photo-1597852074816-d933c7d2b988?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    link: "https://brahimsaa.github.io/Windows_Server_2022",
+    category: "windows administration"
     }
 ];
 
