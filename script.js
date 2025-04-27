af2897021f9552d98d96226d61eda6a0ae3c8b5d
Brahim SAADA <brahim.saada@uit.ac.ma>
2025-04-27 16:36:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -153,6 +153,20 @@ const courses = [
     image: "https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
     link: "https://brahimsaa.github.io/DevOps_Linux",
     category: "devops"
+    },
+    {
+    title: "Bug Bounty Hunting",
+    description: "Learn ethical hacking techniques to find and report vulnerabilities for bug bounty programs.",
+    image: "https://images.unsplash.com/photo-1563986768609-322da13575f3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    link: "https://brahimsaa.github.io/BugBounty",
+    category: "cybersecurity"
+    },
+    {
+    title: "JavaScript Programming",
+    description: "Master JavaScript fundamentals and modern ES6+ features for web development.",
+    image: "https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80",
+    link: "https://brahimsaa.github.io/javascript",
+    category: "web development"
     }
 ];
 
