dc456f5b5526e6c41d47150de535555749243a40
Brahim SAADA <brahim.saada@uit.ac.ma>
2025-07-27 00:20:43 +0100

    Add Next.js Simplified course

--- script.js
+++ script.js
@@ -279,6 +279,13 @@ const courses = [
         "image": "https://d3f1iyfxxz8i1e.cloudfront.net/courses/course_image/9a4da97c3b99.jpg",
         "link": "https://BrahimSAA.github.io/NGINX_Fundamentals_High_Performance_Servers_from_Scratch",
         "category": "web-development"
+    },
+    {
+        "title": "Next.js Simplified",
+        "description": "Learn Next.js quickly and easily with this simplified course, covering essential concepts and best practices to build modern web applications. This course helps you master the fundamentals and build practical projects.",
+        "image": "https://i.ytimg.com/vi/nHnw3pusFXw/mqdefault.jpg",
+        "link": "https://BrahimSAA.github.io/Next-js_Simplified/",
+        "category": "web-development"
     }
 
 ];
