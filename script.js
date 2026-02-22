7872bc0ed3db6509496179b43e4343cf1d56f9f9
Brahim SAADA <brahim.saada@uit.ac.ma>
2026-02-22 21:18:54 +0000

    Add Nmap course

--- script.js
+++ script.js
@@ -331,6 +331,13 @@ const courses = [
         "image": "None",
         "link": "https://BrahimSAA.github.io/Nmap/",
         "category": "cybersecurity"
+    }, 
+    {
+        "title": "Nmap",
+        "description": "Learn the fundamentals of Nmap, a powerful open-source network scanner used for network discovery and security auditing. This course covers various scanning techniques, script usage, and interpreting results to identify vulnerabilities.",
+        "image": "None",
+        "link": "https://BrahimSAA.github.io/Nmap/",
+        "category": "cybersecurity"
     }
 ];
 
